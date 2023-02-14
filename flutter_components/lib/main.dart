a8529d054003fa4ee4079df1667c1c67b85ded84
SimbakSmart <109449618+SimbakSmart@users.noreply.github.com>
2023-02-14 13:57:30 -0700

    15. Rutas generadas dinámicamente y archivo de rutas independiente

--- flutter_components/lib/main.dart
+++ flutter_components/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_components/src/pages/alert_page.dart';
 import 'package:flutter_components/src/pages/avatar_page.dart';
 import 'package:flutter_components/src/pages/home_page.dart';
 import 'package:flutter_components/src/pages/home_temp.dart';
+import 'package:flutter_components/src/routes/routes.dart';
 
 void main() {
   runApp( MyApp());
@@ -19,11 +20,15 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
      // home: HomePage(),
       initialRoute: '/',
-      routes: <String, WidgetBuilder>{
-        '/' :(BuildContext context) =>HomePage(),
-        'alert' :(BuildContext context) =>AlertPage(),
-        'avatar' :(BuildContext context) =>AvatarPage(),
-      }
+      routes:getApplicationRoutes(),
+      onGenerateRoute: (RouteSettings settings){ //Si una pagina no esta configura, muestra alert page pof defecto
+
+        print( 'Ruta llamda: ${ settings.name }' );
+
+        return MaterialPageRoute(
+            builder: ( BuildContext context ) => AlertPage()
+        );
+      },
     );
   }
 }
---
+++ flutter_components/lib/src/routes/routes.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:flutter/cupertino.dart';
+
+import '../pages/alert_page.dart';
+import '../pages/avatar_page.dart';
+import '../pages/home_page.dart';
+
+Map<String, WidgetBuilder> getApplicationRoutes() {
+
+  return <String, WidgetBuilder> {
+    '/'      : ( BuildContext context ) => HomePage(),
+    'alert'  : ( BuildContext context ) => AlertPage(),
+    //AvatarPage.pageName : ( BuildContext context ) => AvatarPage(),
+
+  };
+
+}
