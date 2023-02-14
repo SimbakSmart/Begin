2a3dc952736e21cee87f4cb982059c5bf212c3d6
SimbakSmart <109449618+SimbakSmart@users.noreply.github.com>
2023-02-14 11:45:31 -0700

    8. Página - HomePage

--- flutter_components/lib/main.dart
+++ flutter_components/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_components/src/pages/home_page.dart';
 import 'package:flutter_components/src/pages/home_temp.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Componentes APP',
       debugShowCheckedModeBanner: false,
-      home: HomePageTemp(),
+      home: HomePage(),
     );
   }
 }
---
+++ flutter_components/lib/src/pages/home_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes'),
+      ),
+      body: _lista(),
+    );
+  }
+
+  Widget _lista(){
+     return ListView(
+       children: _listaItems(),
+     );
+  }
+
+  List<Widget> _listaItems() {
+
+    return [
+      ListTile(title: Text('Hola Mundo')),
+      Divider(),
+      ListTile(title: Text('Hola Mundo')),
+      Divider(),
+      ListTile(title: Text('Hola Mundo')),
+      Divider(),
+      ListTile(title: Text('Hola Mundo')),
+      Divider(),
+    ];
+  }
+}
