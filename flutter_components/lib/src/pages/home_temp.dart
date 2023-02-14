efa9dcd797014d7300b481a06d237fb78dd97ec8
SimbakSmart <109449618+SimbakSmart@users.noreply.github.com>
2023-02-14 11:38:24 -0700

    7. Método map de las listas y ListTile

--- flutter_components/lib/src/pages/home_temp.dart
+++ flutter_components/lib/src/pages/home_temp.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class HomePageTemp extends StatelessWidget {
 
   final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -10,25 +11,41 @@ class HomePageTemp extends StatelessWidget {
       appBar: AppBar(
         title: Text('Componentes Temp'),
       ),
-      body:ListView(
-        children: _crearItems(),
+      body: ListView(
+        children: _crearItemsCorta(),
       ),
     );
   }
 
   List<Widget> _crearItems() {
-
     List<Widget> lista = <Widget>[];
 
     for (String opt in opciones) {
-
       final tempWidget = ListTile(
-        title: Text( opt ),
+        title: Text(opt),
       );
 
-      lista..add( tempWidget )
-        ..add( Divider() );
+      lista..add(tempWidget)..add(Divider());
     }
     return lista;
   }
+
+
+  List<Widget> _crearItemsCorta() {
+    return opciones.map((item) {
+      return Column(
+        children: [
+          ListTile(
+            title: Text(item + '!'),
+            subtitle: Text('Cualquier cosa'),
+            leading: Icon(Icons.account_balance_wallet),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: () {},
+          ),
+          Divider()
+        ],
+      );
+    }).toList();
+  }
+
 }
