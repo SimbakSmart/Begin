18f132f6e4f85fa9425feadbf743900f044ff1b6
SimbakSmart <109449618+SimbakSmart@users.noreply.github.com>
2023-02-14 14:09:57 -0700

    16. Card Widget

--- flutter_components/lib/src/routes/routes.dart
+++ flutter_components/lib/src/routes/routes.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 
 import '../pages/alert_page.dart';
 import '../pages/avatar_page.dart';
+import '../pages/card_page.dart';
 import '../pages/home_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
@@ -11,7 +12,8 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
   return <String, WidgetBuilder> {
     '/'      : ( BuildContext context ) => HomePage(),
     'alert'  : ( BuildContext context ) => AlertPage(),
-    //AvatarPage.pageName : ( BuildContext context ) => AvatarPage(),
+    AvatarPage.pageName : ( BuildContext context ) => AvatarPage(),
+    'card'   : ( BuildContext context ) => CardPage(),
 
   };
 
