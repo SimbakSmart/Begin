4ff716cdcab06b364a21a3aa65edea2747221771
SimbakSmart <109449618+SimbakSmart@users.noreply.github.com>
2023-02-14 14:39:04 -0700

    20. Widget - Alert

--- flutter_components/lib/src/pages/alert_page.dart
+++ flutter_components/lib/src/pages/alert_page.dart
@@ -2,19 +2,26 @@ import 'package:flutter/material.dart';
 
 class AlertPage extends StatelessWidget {
 
-
+  final ButtonStyle style =
+  ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20,color: Colors.white));
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
 
       appBar: AppBar(
-        title: Text('Alert Page'),
-        centerTitle: true,
+        title: Text('Alert Page')
       ),
       body: Center(
 
-        child:Text('Alert page') ,
+        child: ElevatedButton(
+          child: Text('Mostrar Alerta'),
+          style: style,
+          // color: Colors.blue,
+          // textColor: Colors.white,
+          // shape: StadiumBorder(),
+          onPressed: () => _mostrarAlert(context),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon( Icons.add_location ),
@@ -24,4 +31,43 @@ class AlertPage extends StatelessWidget {
       ),
     );
   }
+
+  void _mostrarAlert(BuildContext context) {
+
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+
+          return AlertDialog(
+            shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(20.0) ),
+            title: Text('Titulo'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text('Este es el contenido de la caja de la alerta'),
+                FlutterLogo( size: 100.0 )
+              ],
+            ),
+            actions: <Widget>[
+              ElevatedButton(
+                child: Text('Cancelar'),
+                onPressed: ()=> Navigator.of(context).pop(),
+              ),
+              ElevatedButton(
+                child: Text('Ok'),
+                onPressed: (){
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+
+        }
+
+    );
+
+
+  }
+
 }
