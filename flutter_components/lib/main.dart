d80d5b418b9a6fb93ce4297263561627847a2dca
SimbakSmart <109449618+SimbakSmart@users.noreply.github.com>
2023-02-14 10:52:32 -0700

    4. Inicio de proyecto - Componentes

---
+++ flutter_components/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_components/src/pages/home_temp.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: 'Componentes APP',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+
+        body: HomePageTemp(),
+      ),
+    );
+  }
+}
+
+
